ad01d89ca871dc256c91196d080abab9bfca195f
theoHC <theo_coulson@me.com>
2017-08-04 11:39:03 -0400

    Add files via upload

---
+++ LTCS/LTCS.ino
@@ -0,0 +1,61 @@
+/* Transmitter Code
+*  Code to read Joystick position and transmit it with a RF24L01+ to a receiver 
+*/
+
+#include <SPI.h>
+#include <RF24.h>
+
+// Radio Configuration
+
+RF24 radio(9,10); //sets CE and CSN pins to 9 & 10
+
+byte addresses[][6] = {"LTa","LTb"}; //sets addresses for communication
+//change these values to have unique communication! they can be anything
+
+bool radioNumber=1;
+bool role = 1;  //Control transmit 1/receive 0
+
+//Analog joystick pins
+int p1 = 0;       
+int p2 = 1;      
+int p3 = 2;  
+int p4 = 3;
+
+struct package{
+   int leftX ;             // create struct for data package
+   int leftY ;   
+   int rightY ; 
+   int fire ;   
+} go;                  //initialize variable of struct type
+
+void setup() {
+    Serial.begin(9600);                   // Get ready to send data back for debugging purposes
+    radio.begin();                        // Get the transmitter ready
+    radio.setPALevel(RF24_PA_LOW);        // Set the power to low
+    radio.openWritingPipe(addresses[1]);  // Where we send data out
+    radio.openReadingPipe(1,addresses[0]); // Where we receive data back
+    radio.setPayloadSize(32);  
+//Sets payload size, however this is the default of the library. 
+// May want to make value smaller if you are sending a small amount of data per packet
+}
+
+void loop() {
+    //go.leftX = analogRead(0);//map(analogRead(0), 0, 1023, -255, 255);             // Read the joystick values, puts values into package variables
+    //go.leftY = analogRead(1);//map(analogRead(1), 0, 1023, -255, 255);   
+    //go.rightY = analogRead(2);//map(analogRead(2), 0, 1023, -255, 255); 
+    //go.fire = digitalRead(2); 
+    go.leftY = map(analogRead(p1), 0, 1023, -255, 255) * -1;             // Read the joystick values, puts values into package variables
+    go.leftX = map(analogRead(p2), 0, 1023, 5, -5);   
+    go.rightY = map(analogRead(p3), 0, 1023, -255, 255); 
+    go.fire = abs(map(analogRead(p4), 0, 1023, -5, 5)); 
+
+    Serial.print(go.leftY);
+    Serial.print(go.rightY);
+    radio.stopListening();                                 // Stop listening and begin transmitting
+    delay(10);                                            // to stop jittering data
+
+    if(radio.write(&go, sizeof(go)),Serial.println("SENT"));  // writes values to receiver, prints confirmation
+ 
+    radio.startListening();   //listens for receiver's confirmation
+}
+
---
+++ LTOS/LTOS.ino
@@ -0,0 +1,117 @@
+#include <SPI.h>
+#include <RF24.h>
+#include <Servo.h>
+
+Servo turretServo;
+
+//Radio Configuration
+
+RF24 radio(7,10);
+byte addresses[][6] = {"LTa1","LTb1"}; // make sure this matches the transmitter adresses
+
+bool radioNumber=0;
+bool role = 0;  //Control transmit/receive
+
+int leftSensor;
+int rightSensor;
+int turretAngle = 90;
+int health = 255;
+
+struct package{
+   int leftX ;             // create struct for data package
+   int leftY ;   
+   int rightY ; 
+   int fire ;   
+} go;        
+
+void setup() { 
+  Serial.begin(9600);        
+  radio.begin();             // Initializes radio
+  radio.setPALevel(RF24_PA_LOW);   // Sets power output to low.
+  radio.openWritingPipe(addresses[0]); //opens writing pipeline
+  radio.openReadingPipe(1,addresses[1]); //opens reading pipeline
+  radio.setPayloadSize(32); //sets payload size, but this is default
+  radio.startListening();
+
+  pinMode(8,OUTPUT);       //Channel A Brake Pin Initialize
+  pinMode(9,OUTPUT);       //Channel B Brake Pin Initialize 
+  pinMode(4,OUTPUT);      //Channel A Direction Pin Initialize
+  pinMode(2,OUTPUT);      //Channel B Direction Pin Initialize
+  pinMode(3, OUTPUT);      //Channel A Speed Pin Initialize
+  pinMode(6, OUTPUT);     //Channel B Speed Pin Initialize
+
+  turretServo.attach(5);
+}
+void loop() {  
+  delay(20);               //increase for debuggy, decrease to decrease jitter
+  if(radio.available()){
+    radio.read(&go,sizeof(go)); //reads value of go
+  } else { 
+    Serial.println("NO RADIO"); 
+  }
+/*
+  Serial.print(go.leftY);    //prints all 4 values from joysticks
+  Serial.print("\t");
+  Serial.print(go.leftX);
+  Serial.print("\t");
+  Serial.print(go.rightY);
+  Serial.print("\t");
+  Serial.print(go.fire);
+  Serial.print("\t");
+  Serial.println(turretAngle);
+  delay(20);
+  */
+  //LASER CODE
+  if(go.fire > 1){
+    analogWrite(A5, 255);
+  }else{
+    digitalWrite(A5, 0);
+  }
+  leftSensor = analogRead(A3);
+  rightSensor = analogRead(A4);
+  if(leftSensor >= rightSensor + 75 || rightSensor >= leftSensor + 75){
+    digitalWrite(15, HIGH);
+    Serial.println("LASER");
+    analogWrite(A5, 255);
+  }
+  Serial.println(rightSensor);
+  Serial.println(leftSensor);
+
+  //MOTOR CODE
+  digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
+  if(go.leftY >= 0){
+    digitalWrite(4, LOW);
+  }else{
+    digitalWrite(4, HIGH);
+  }
+  if(go.rightY >= 0){
+    digitalWrite(2, LOW);
+  }else{
+    digitalWrite(2, HIGH);
+  }
+  if(radio.available()){
+    analogWrite(3, abs(go.leftY)/2);
+    analogWrite(6, abs(go.rightY)/2);
+  }else{
+
+    
+    analogWrite(3, 0);
+    analogWrite(6, 0);
+  }
+
+  //SERVO CODE
+  if(go.leftX > 2 && turretAngle < 180 && radio.available()){
+    turretAngle += 2;
+  }
+  if(go.leftX < -2 && turretAngle > 0 && radio.available()){
+    turretAngle -= 2;
+  }
+  if(radio.available()){
+    turretServo.write(turretAngle);
+  }else{
+    turretServo.write(90);
+    turretAngle = 90;
+  }
+}
+
